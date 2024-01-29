66aeada3e93808e2fd7415d6a6c321152b9bc933
Wemilly Souza <wemillymaria40@gmail.com>
2024-01-29 17:47:09 -0300

    Adicionando frontend da aplicação

---
+++ client/main.py
@@ -0,0 +1,53 @@
+import requests
+
+class Hotel:
+    def __init__(self):
+        self.quartos = {}
+
+    def listar_quartos(self):
+        quartos = requests.get("http://localhost:8900/rooms/")
+        quartos = quartos.json()["rooms"]
+        print(quartos)
+    def check_in(self, numero_quarto):
+        check_in = requests.patch(f"http://localhost:8900/rooms/{numero_quarto}/checkin/")
+        if check_in.status_code==200:
+            check_in=check_in.json()
+            print(check_in["detail"])
+        else: 
+            print("Não foi possível efetuar o check-in.") 
+
+    def check_out(self, numero_quarto):
+        check_out = requests.patch(f"http://localhost:8900/rooms/{numero_quarto}/checkout/")
+        if check_out.status_code==200:
+            check_out=check_out.json()
+            print(check_out["detail"]) 
+        else: 
+            print("Não foi possível efetuar o check-out.") 
+def main():
+    hotel = Hotel()
+
+    while True:
+        print("\nSeja bem-vindo ao Hotel! ")
+        print("1 - Listar quartos")
+        print("2 - Check-in no quarto")
+        print("3 - Checkout no quarto")
+        print("4 - Sair")
+
+        escolha = input("Escolha uma opção (1/2/3/4): ")
+
+        if escolha == "1":
+            hotel.listar_quartos()
+        elif escolha == "2":
+            numero_quarto = input("Digite o número do quarto para check-in: ")
+            hotel.check_in(numero_quarto)
+        elif escolha == "3":
+            numero_quarto = input("Digite o número do quarto para check-out: ")
+            hotel.check_out(numero_quarto)
+        elif escolha == "4":
+            print("Saindo do programa. Até logo!")
+            break
+        else:
+            print("Opção inválida. Por favor, escolha uma opção válida.")
+
+if __name__ == "__main__":
+    main()
